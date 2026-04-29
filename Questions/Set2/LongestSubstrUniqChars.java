94c9a7e06a0b4bca2b6cf51069d4733f7b3dfa2a
Vansh Gugliani <vanshgugliani@gmail.com>
2026-04-29 19:35:31 +0530

    Set 2 Added

---
+++ Questions/Set2/LongestSubstrUniqChars.java
@@ -0,0 +1,32 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int longest = 0;
+        int left = 0;
+        Set<Character> set = new HashSet<>();
+        for(int right=0;right<s.length();right++){
+            char c = s.charAt(right);
+
+            while(set.contains(c)){
+                set.remove(s.charAt(left));
+                left++;
+            }
+            set.add(c);
+            longest = Math.max(longest,right-left+1);
+        }
+        return longest;
+    }
+}
+public class LongestSubstrUniqChars {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        String s = "pwwkew";  // test case
+
+        int result = sol.lengthOfLongestSubstring(s);
+
+        System.out.println("Length of longest substring without repeating characters: " + result);
+    }
+}
